89e63e8db29e80ad8c1609098e68ad64ef710e49
Demchenko Kirill <thegalaxydias@gmail.com>
2021-09-28 19:41:31 +0300

    Добавьте файлы проекта.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string file = "files.txt";
+                string path = Path.GetFullPath(file);
+                string a = File.ReadAllText(path);
+                Console.WriteLine(a);
+            }
+
+            catch
+            {
+                Console.WriteLine("Please create file!");
+            }
+
+            finally
+            {
+                Console.WriteLine("Operation is finished");
+            }
+        }
+    }
+}
